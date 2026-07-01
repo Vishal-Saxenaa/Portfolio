0b173ff784b74f47b09410be6f6288847887cc69
vishal <saxenaji789@gmail.com>
2026-07-01 23:39:55 +0530

    My portfolio

---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,80 @@
+import "./Projects.css"
+
+function Projects(){
+  return(
+    <section className="projects" id="projects">
+      <div className="project-container">
+        <h2 className="project-title">
+          Featured Projects
+        </h2>
+
+        <p className="project-subtitle">
+          A collection of modern MERN Stack and AI-based applications that showcase my skills in full-stack development, problem-solving, and building real-world solutions.
+        </p>
+
+        <div className="projects-grid">
+          {projects.map((project) => (
+            <div className="project-card" key={project.id}>
+             
+              <div className="project-image">
+                Project Image
+              </div>
+             
+              <h3>{project.title}</h3>
+             
+              <p>{project.discription}</p>
+            
+            </div>
+          
+          ))}
+        </div>
+      
+        </div>
+
+    </section>
+  );
+}
+
+const projects =[
+  {
+   id: 1,
+    title: "URL Shortener",
+    description:
+      "A modern URL Shortener with authentication, QR code generation, and click analytics.",
+    tech: ["React", "Node.js", "Express", "MongoDB"],
+    github: "#",
+    live: "#",
+  },
+
+  {
+    id: 2,
+    title: "Multi Utility Widget Hub",
+    description:
+      "A collection of useful widgets like Calculator, BMI, EMI, Weather, QR Code, and more.",
+    tech: ["React", "Node.js", "MongoDB"],
+    github: "#",
+    live: "#",
+  },
+
+  {
+    id: 3,
+    title: "AI Project",
+    description:
+      "An AI-powered MERN application using modern AI APIs to solve real-world problems.",
+    tech: ["React", "Node.js", "AI API"],
+    github: "#",
+    live: "#",
+  },
+
+  {
+    id: 4,
+    title: "Library / Coaching Management",
+    description:
+      "A complete management system with authentication, dashboard, attendance, and reports.",
+    tech: ["React", "Express", "MongoDB"],
+    github: "#",
+    live: "#",
+  },
+] ;
+
+export default Projects;
